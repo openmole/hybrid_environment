0309b2748f70c7018ed33e442d0283230afa90a7
Hoel Kervadec <hk3315@romeo.doc.ic.ac.uk>
2015-07-08 16:04:09 +0100

    A little bit of work to measure the upload time

--- src/main/scala/environment_listener/EnvListener.scala
+++ src/main/scala/environment_listener/EnvListener.scala
@@ -1,15 +1,30 @@
 package environment_listener
 
 import org.openmole.core.batch.environment.BatchEnvironment
+import org.openmole.core.batch.environment.BatchEnvironment.{BeginUpload, EndUpload}
 import org.openmole.core.tools.service.Logger
 import org.openmole.core.event._
+import org.openmole.core.event.Event
+import org.openmole.core.workflow.mole.MoleExecution.JobStatusChanged
+
+import scala.collection.mutable.HashMap
 
 class EnvListener(env : BatchEnvironment) extends Runnable{
+    val transferStarts = new HashMap[Long, Long]
 
     def run: Unit = {
-        Listener.Log.logger.fine(s"Start to monitor the environment $env")
+        Listener.Log.logger.info(s"Start to monitor the environment $env")
 
         env listen {
+            case (_ , BeginUpload(id, path, stor)): (Any, Event[BatchEnvironment])) => {
+//                println(s"Begin: $path $stor")
+                transferStarts.put(id, System.currentTimeMillis())
+            }
+            case (_, EndUpload(id, path, stor)) => {
+//                println(s"Begin: $path $stor")
+                val v = System.currentTimeMillis() - transferStarts(id)
+                Listener.put_value(env, id, "upload_time", v)
+            }
             case _ => println("I'm hearing thingsssss")
         }
     }
--- src/main/scala/environment_listener/Listener.scala
+++ src/main/scala/environment_listener/Listener.scala
@@ -9,12 +9,40 @@ import scala.collection.mutable.HashMap
 object Listener extends Logger{
     // WHY on earth this is not working with concurrent.Map
     // FIXME concurrent map
-	var data_store = HashMap[BatchEnvironment, String]()
+	private var data_store = HashMap[BatchEnvironment, HashMap[Long, HashMap[String, Any]]]()
 
-	def add_env(env: BatchEnvironment) = {
-		data_store(env) = "toto"
+
+    /**
+     * Register a new environment that will be listened to
+     * Still need to call start_monitoring to actually start to listen
+     * @param env The environment to listen
+     */
+	def register_env(env: BatchEnvironment) = {
+		data_store(env) = HashMap[Long, HashMap[String, Any]]()
 	}
 
+
+    /**
+     * Will write a value in the data_store
+     * @param env The environment where the job is running
+     * @param job_id The job id
+     * @param m The metric name
+     * @param v The actual value
+     */
+    def put_value(env : BatchEnvironment, job_id : Long, m : String, v : Any) = {
+        if(!data_store(env).keySet.contains(job_id)){
+            data_store(env)(job_id) = HashMap[String, Any]()
+
+            // Retrieve in openmole the job caracteristics ?
+        }
+
+        data_store(env)(job_id)(m) = v
+    }
+
+
+    /**
+     * Launch a new thread of EnvListener for each environment registered
+     */
 	def start_monitoring() = {
         for(env <- data_store.keys){
             new EnvListener(env).run()
@@ -22,9 +50,19 @@ object Listener extends Logger{
 	}
 
 
+    /**
+     * Will print all the data contained in the data_store
+     * Should be replaced by a function writing everything in a file
+     */
 	def print_datas() = {
-		for(key <- data_store.keys()){
-			println(key)
+		for(env : BatchEnvironment <- data_store.keys){
+			println(s"Env: $env")
+            for(job : Long <- data_store(env).keys) {
+                println(s"\tJob: $job")
+                for(metric : String <- data_store(env)(job).keys){
+                    println(s"\t\t$metric : ${data_store(env)(job)(metric)}")
+                }
+            }
 		}
 	}
 }
